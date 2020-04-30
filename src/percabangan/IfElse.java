c589cd044a46681721609330ff32bb90a08650e4
aqshalms <aqshalms48@gmail.com>
2020-05-01 00:41:08 +0800

    Percabangan IfElse Condition bernilai False dan Statement 2 jalan

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -2,13 +2,13 @@ package percabangan;
 
 public class IfElse {
     public static void main(String[] args) {
-        double totalBelanja = 53000;
-        double uangDiDompet = 25000;
+        double totalBelanja = 50000;
+        double uangDiDompet = 60000;
 
         if (uangDiDompet<totalBelanja){
             System.out.println("Uang kurang, kurangi jajan anda");
         }else{
-            System.out.println("Uang cukup, selamat menikmati");
+            System.out.println("Uang cukup, terima kasih");
         }
     }
 }
