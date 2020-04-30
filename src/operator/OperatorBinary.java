bf6459755ab332568445c6859181b8ca9f1f876b
aqshalms <aqshalms48@gmail.com>
2020-05-01 00:21:19 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,14 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assignment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Muhammad Aqshal Maulana Saputra";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+
+}
+
