2603ce2a001ac42b7fb421f1adb256cb620368c0
aqshalms <aqshalms48@gmail.com>
2020-05-01 00:52:16 +0800

    Percabangan Switch

---
+++ src/percabangan/Switch.java
@@ -0,0 +1,24 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int jalanJalanKe=3;
+        switch (jalanJalanKe){
+            case 1:
+                System.out.println("Jakarta");
+                break;
+            case 2:
+                System.out.println("Banjarmasin");
+                break;
+            case 3:
+                System.out.println("Yogyakarta");
+                break;
+            case 4:
+                System.out.println("Surabaya");
+                break;
+            default:
+                System.out.println("Ga Kemana Mana");
+                break;
+        }
+    }
+}
